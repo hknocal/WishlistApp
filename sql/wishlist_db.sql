bbea7667bc6ce1f4a0fa0e19fe638645de633f07
hknocal <hknocal@outlook.com>
2023-04-10 16:37:15 +0200

    inserted randomized dummy data for user, wishlist and item tables

--- sql/wishlist_db.sql
+++ sql/wishlist_db.sql
@@ -3,60 +3,102 @@ CREATE DATABASE wishlist_db;
 USE wishlist_db;
 
 CREATE TABLE user (
-id INT NOT NULL AUTO_INCREMENT,
-first_name VARCHAR(255) NOT NULL,
-last_name VARCHAR(255),
-email VARCHAR(255),
-PRIMARY KEY (id)
+					  id INT NOT NULL AUTO_INCREMENT,
+					  first_name VARCHAR(255) NOT NULL,
+					  last_name VARCHAR(255),
+					  email VARCHAR(255),
+					  PRIMARY KEY (id)
 );
 
 CREATE TABLE wishlist (
-id INT NOT NULL AUTO_INCREMENT,
-wishlist_title VARCHAR(255),
-user_id int NOT NULL,
-PRIMARY KEY (id),
-FOREIGN KEY (user_id) REFERENCES user(id)
+						  id INT NOT NULL AUTO_INCREMENT,
+						  wishlist_title VARCHAR(255),
+						  user_id int NOT NULL,
+						  PRIMARY KEY (id),
+						  FOREIGN KEY (user_id) REFERENCES user(id)
 );
 
 CREATE TABLE item (
-id INT NOT NULL AUTO_INCREMENT,
-item_name VARCHAR(255) NOT NULL,
-item_link VARCHAR(255),
-item_price INT,
-item_quantity INT,
-wishlist_id int NOT NULL,
-PRIMARY KEY (id),
-FOREIGN KEY (wishlist_id) REFERENCES wishlist(id)
+					  id INT NOT NULL AUTO_INCREMENT,
+					  item_name VARCHAR(255) NOT NULL,
+					  item_link VARCHAR(255),
+					  item_price INT,
+					  item_quantity INT,
+					  wishlist_id int NOT NULL,
+					  PRIMARY KEY (id),
+					  FOREIGN KEY (wishlist_id) REFERENCES wishlist(id)
 );
 
-insert into user (first_name, last_name, email) values ('Liane', 'Risen', 'lrisen0@tamu.edu');
-insert into user (first_name, last_name, email) values ('Cammi', 'Gillian', 'cgillian1@noaa.gov');
-insert into user (first_name, last_name, email) values ('Fernanda', 'Dursley', 'fdursley2@altervista.org');
-insert into user (first_name, last_name, email) values ('Philippe', 'Gethin', 'pgethin3@nationalgeographic.com');
-insert into user (first_name, last_name, email) values ('Laurens', 'Marples', 'lmarples4@comcast.net');
-insert into user (first_name, last_name, email) values ('Edward', 'Jenicek', 'ejenicek5@wp.com');
-insert into user (first_name, last_name, email) values ('Margette', 'Stearn', 'mstearn6@economist.com');
-insert into user (first_name, last_name, email) values ('Morry', 'Halfhead', 'mhalfhead7@trellian.com');
-insert into user (first_name, last_name, email) values ('Gunilla', 'Filippucci', 'gfilippucci8@wufoo.com');
-insert into user (first_name, last_name, email) values ('Jone', 'Gilbee', 'jgilbee9@google.ru');
-insert into user (first_name, last_name, email) values ('Camila', 'Hrachovec', 'chrachoveca@oakley.com');
-insert into user (first_name, last_name, email) values ('Analise', 'Dimsdale', 'adimsdaleb@cyberchimps.com');
-insert into user (first_name, last_name, email) values ('Saunderson', 'Domican', 'sdomicanc@icq.com');
-insert into user (first_name, last_name, email) values ('Prudy', 'Flag', 'pflagd@aboutads.info');
-insert into user (first_name, last_name, email) values ('Elora', 'Dicty', 'edictye@about.me');
-insert into user (first_name, last_name, email) values ('Kania', 'Lowings', 'klowingsf@privacy.gov.au');
-insert into user (first_name, last_name, email) values ('Tonnie', 'Sutor', 'tsutorg@gnu.org');
-insert into user (first_name, last_name, email) values ('Winfield', 'Heephy', 'wheephyh@woothemes.com');
-insert into user (first_name, last_name, email) values ('Jo-anne', 'McCroary', 'jmccroaryi@blinklist.com');
-insert into user (first_name, last_name, email) values ('Giovanna', 'Bellchamber', 'gbellchamberj@vinaora.com');
+INSERT INTO USER (first_name, last_name, email) VALUES ('Person A', 'Surname A', 'persona@randomdomain.com');
+INSERT INTO USER (first_name, last_name, email) VALUES ('Person B', 'Surname B', 'personb@randomdomain.com');
+INSERT INTO USER (first_name, last_name, email) VALUES ('Person C', 'Surname C', 'personc@randomdomain.com');
+INSERT INTO USER (first_name, last_name, email) VALUES ('Person D', 'Surname D', 'persond@randomdomain.com');
+INSERT INTO USER (first_name, last_name, email) VALUES ('Person E', 'Surname E', 'persone@randomdomain.com');
+INSERT INTO USER (first_name, last_name, email) VALUES ('Person F', 'Surname F', 'personf@randomdomain.com');
+INSERT INTO USER (first_name, last_name, email) VALUES ('Person G', 'Surname G', 'persong@randomdomain.com');
+INSERT INTO USER (first_name, last_name, email) VALUES ('Person H', 'Surname H', 'personh@randomdomain.com');
+INSERT INTO USER (first_name, last_name, email) VALUES ('Person I', 'Surname I', 'personi@randomdomain.com');
+INSERT INTO USER (first_name, last_name, email) VALUES ('Person J', 'Surname J', 'personj@randomdomain.com');
+
+INSERT INTO wishlist(wishlist_title, user_id) VALUES ('Wishlist ABCDF', 1);
+INSERT INTO wishlist(wishlist_title, user_id) VALUES ('Wishlist WXYZA', 1);
+INSERT INTO wishlist(wishlist_title, user_id) VALUES ('Wishlist LMNOE', 2);
+INSERT INTO wishlist(wishlist_title, user_id) VALUES ('Wishlist RSTUX', 2);
+INSERT INTO wishlist(wishlist_title, user_id) VALUES ('Wishlist MNOPF', 3);
+INSERT INTO wishlist(wishlist_title, user_id) VALUES ('Wishlist ENOPF', 3);
+INSERT INTO wishlist(wishlist_title, user_id) VALUES ('Wishlist IJKLH', 4);
+INSERT INTO wishlist(wishlist_title, user_id) VALUES ('Wishlist WXYZU', 4);
+INSERT INTO wishlist(wishlist_title, user_id) VALUES ('Wishlist BCDEG', 5);
+INSERT INTO wishlist(wishlist_title, user_id) VALUES ('Wishlist JKLWD', 5);
+INSERT INTO wishlist(wishlist_title, user_id) VALUES ('Wishlist LMNRR', 6);
+INSERT INTO wishlist(wishlist_title, user_id) VALUES ('Wishlist GMNRR', 6);
+INSERT INTO wishlist(wishlist_title, user_id) VALUES ('Wishlist DEFGR', 7);
+INSERT INTO wishlist(wishlist_title, user_id) VALUES ('Wishlist TUVWR', 7);
+INSERT INTO wishlist(wishlist_title, user_id) VALUES ('Wishlist QRSHT', 8);
+INSERT INTO wishlist(wishlist_title, user_id) VALUES ('Wishlist VWXYB', 8);
+INSERT INTO wishlist(wishlist_title, user_id) VALUES ('Wishlist EFGHN', 9);
+INSERT INTO wishlist(wishlist_title, user_id) VALUES ('Wishlist MNOPV', 9);
+INSERT INTO wishlist(wishlist_title, user_id) VALUES ('Wishlist PQRSY', 10);
+INSERT INTO wishlist(wishlist_title, user_id) VALUES ('Wishlist IJKYH', 10);
 
 
-INSERT INTO wishlist(wishlist_title, user_id)
-VALUES	('Mommys birthday', 1),
-		('Sisters birthday', 2),
-		('Grannys birthday', 3);
-
-INSERT INTO item (item_name, item_link, item_price, item_quantity, wishlist_id)
-VALUES	('Coffee cups', 'https://somerandomsite.com/1234', 184, 3, 1),
-		('Iphone 12', 'https://somerandomsite.com/1234', 799, 0, 2),
-		('Giftcard', 'https://somerandomsite.com/1234', 249, 0, 3);
+INSERT INTO item (item_name, item_link, item_price, item_quantity, wishlist_id) VALUES ('Item ABCD', 'https://randomlink.com/abcd', 1234, 1, 1);
+INSERT INTO item (item_name, item_link, item_price, item_quantity, wishlist_id) VALUES ('Item DEFG', 'https://randomlink.com/defg', 4567, 2, 1);
+INSERT INTO item (item_name, item_link, item_price, item_quantity, wishlist_id) VALUES ('Item GHJI', 'https://randomlink.com/ghji', 8901, 3, 2);
+INSERT INTO item (item_name, item_link, item_price, item_quantity, wishlist_id) VALUES ('Item XYZA', 'https://randomlink.com/xyza', 3456, 1, 2);
+INSERT INTO item (item_name, item_link, item_price, item_quantity, wishlist_id) VALUES ('Item BCDE', 'https://randomlink.com/bcde', 6789, 2, 3);
+INSERT INTO item (item_name, item_link, item_price, item_quantity, wishlist_id) VALUES ('Item FGHI', 'https://randomlink.com/fghi', 0123, 3, 3);
+INSERT INTO item (item_name, item_link, item_price, item_quantity, wishlist_id) VALUES ('Item JKLM', 'https://randomlink.com/jklm', 4567, 1, 4);
+INSERT INTO item (item_name, item_link, item_price, item_quantity, wishlist_id) VALUES ('Item NOPQ', 'https://randomlink.com/nopq', 8901, 2, 4);
+INSERT INTO item (item_name, item_link, item_price, item_quantity, wishlist_id) VALUES ('Item RSTU', 'https://randomlink.com/rstu', 2345, 3, 5);
+INSERT INTO item (item_name, item_link, item_price, item_quantity, wishlist_id) VALUES ('Item VWXY', 'https://randomlink.com/vwxy', 1234, 1, 5);
+INSERT INTO item (item_name, item_link, item_price, item_quantity, wishlist_id) VALUES ('Item ZABC', 'https://randomlink.com/zabc', 5678, 2, 6);
+INSERT INTO item (item_name, item_link, item_price, item_quantity, wishlist_id) VALUES ('Item DEFG', 'https://randomlink.com/defg', 9012, 3, 6);
+INSERT INTO item (item_name, item_link, item_price, item_quantity, wishlist_id) VALUES ('Item HIJK', 'https://randomlink.com/hijk', 2345, 1, 7);
+INSERT INTO item (item_name, item_link, item_price, item_quantity, wishlist_id) VALUES ('Item LMNO', 'https://randomlink.com/lmno', 6789, 2, 7);
+INSERT INTO item (item_name, item_link, item_price, item_quantity, wishlist_id) VALUES ('Item PQRS', 'https://randomlink.com/pqrs', 0123, 3, 8);
+INSERT INTO item (item_name, item_link, item_price, item_quantity, wishlist_id) VALUES ('Item TUVW', 'https://randomlink.com/tuvw', 4567, 1, 8);
+INSERT INTO item (item_name, item_link, item_price, item_quantity, wishlist_id) VALUES ('Item XYZA', 'https://randomlink.com/xyza', 8901, 2, 9);
+INSERT INTO item (item_name, item_link, item_price, item_quantity, wishlist_id) VALUES ('Item BCDE', 'https://randomlink.com/bcde', 2345, 3, 9);
+INSERT INTO item (item_name, item_link, item_price, item_quantity, wishlist_id) VALUES ('Item FGHI', 'https://randomlink.com/fghi', 5678, 1, 10);
+INSERT INTO item (item_name, item_link, item_price, item_quantity, wishlist_id) VALUES ('Item JKLM', 'https://randomlink.com/jklm', 9012, 2, 10);
+INSERT INTO item (item_name, item_link, item_price, item_quantity, wishlist_id) VALUES ('Item NOPQ', 'https://randomlink.com/nopq', 3456, 3, 11);
+INSERT INTO item (item_name, item_link, item_price, item_quantity, wishlist_id) VALUES ('Item RSTU', 'https://randomlink.com/rstu', 7890, 1, 11);
+INSERT INTO item (item_name, item_link, item_price, item_quantity, wishlist_id) VALUES ('Item VWXY', 'https://randomlink.com/vwxy', 1234, 2, 12);
+INSERT INTO item (item_name, item_link, item_price, item_quantity, wishlist_id) VALUES ('Item ZABC', 'https://randomlink.com/zabc', 5678, 3, 12);
+INSERT INTO item (item_name, item_link, item_price, item_quantity, wishlist_id) VALUES ('Item LMNO', 'https://randomlink.com/lmno', 2345, 1, 13);
+INSERT INTO item (item_name, item_link, item_price, item_quantity, wishlist_id) VALUES ('Item PQRS', 'https://randomlink.com/pqrs', 6789, 2, 13);
+INSERT INTO item (item_name, item_link, item_price, item_quantity, wishlist_id) VALUES ('Item TUVW', 'https://randomlink.com/tuvw', 2123, 3, 14);
+INSERT INTO item (item_name, item_link, item_price, item_quantity, wishlist_id) VALUES ('Item XYZA', 'https://randomlink.com/xyza', 4567, 1, 14);
+INSERT INTO item (item_name, item_link, item_price, item_quantity, wishlist_id) VALUES ('Item BCDE', 'https://randomlink.com/bcde', 8901, 2, 15);
+INSERT INTO item (item_name, item_link, item_price, item_quantity, wishlist_id) VALUES ('Item FGHI', 'https://randomlink.com/fghi', 2345, 3, 15);
+INSERT INTO item (item_name, item_link, item_price, item_quantity, wishlist_id) VALUES ('Item ABCD', 'https://randomlink.com/abcd', 1234, 1, 16);
+INSERT INTO item (item_name, item_link, item_price, item_quantity, wishlist_id) VALUES ('Item DEFG', 'https://randomlink.com/defg', 4567, 2, 16);
+INSERT INTO item (item_name, item_link, item_price, item_quantity, wishlist_id) VALUES ('Item GHJI', 'https://randomlink.com/ghji', 8901, 3, 17);
+INSERT INTO item (item_name, item_link, item_price, item_quantity, wishlist_id) VALUES ('Item XYZA', 'https://randomlink.com/xyza', 3456, 1, 17);
+INSERT INTO item (item_name, item_link, item_price, item_quantity, wishlist_id) VALUES ('Item BCDE', 'https://randomlink.com/bcde', 6789, 2, 18);
+INSERT INTO item (item_name, item_link, item_price, item_quantity, wishlist_id) VALUES ('Item FGHI', 'https://randomlink.com/fghi', 0123, 3, 18);
+INSERT INTO item (item_name, item_link, item_price, item_quantity, wishlist_id) VALUES ('Item JKLM', 'https://randomlink.com/jklm', 4567, 1, 19);
+INSERT INTO item (item_name, item_link, item_price, item_quantity, wishlist_id) VALUES ('Item NOPQ', 'https://randomlink.com/nopq', 8901, 2, 19);
+INSERT INTO item (item_name, item_link, item_price, item_quantity, wishlist_id) VALUES ('Item RSTU', 'https://randomlink.com/rstu', 2345, 3, 20);
+INSERT INTO item (item_name, item_link, item_price, item_quantity, wishlist_id) VALUES ('Item VWXY', 'https://randomlink.com/vwxy', 1234, 1, 20);
